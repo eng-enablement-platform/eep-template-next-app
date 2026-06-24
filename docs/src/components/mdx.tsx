96d42690938c86abde7326e8adf5b8f8f43b1a56
Hector Kurtyanek <hkdevwork123@gmail.com>
2026-06-24 06:28:40 +0100

    chore: use stepper

--- docs/src/components/mdx.tsx
+++ docs/src/components/mdx.tsx
@@ -1,5 +1,6 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { Step, Steps } from 'fumadocs-ui/components/steps';
 
 import { File, Files, Folder } from '@/components/files';
 import { ImageZoom } from '@/components/image-zoom';
@@ -10,6 +11,8 @@ export function getMDXComponents(components?: MDXComponents) {
     File,
     Files,
     Folder,
+    Step,
+    Steps,
     img: (props) => (
       <ImageZoom {...(props as Parameters<typeof ImageZoom>[0])} />
     ),
